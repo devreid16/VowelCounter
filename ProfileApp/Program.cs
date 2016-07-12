66fbbb827545c8c7b85e52ac5ada7b441dc8a5e6
Angela Reid <reidang94@gmail.com>
2016-07-12 15:49:24 -0400

    Added vowel counting application

--- ProfileApp/Program.cs
+++ ProfileApp/Program.cs
@@ -8,8 +8,36 @@ namespace ProfileApp
 {
     class Program
     {
+
+        static int VCount(string sentence)
+        {
+            int count = 0;
+
+            // Vowels list
+            var vowels = new List<char>
+                { 'a', 'e', 'i', 'o', 'u', 'y' };                     
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))
+                {
+                    count++;
+                }
+            }
+            
+            return count;            
+
+        }
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("Type a word:");
+            string sentence = Console.ReadLine().ToLower();
+
+            int count = VCount(sentence);
+
+            Console.WriteLine("The vowel count of your sentence is: {0}", count);
+            Console.ReadLine();
         }
     }
 }
